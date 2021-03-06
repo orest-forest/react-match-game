449bdcc54691118eca445322e3eb6b347942673b
orest-forest <danilchenko.orest@gmail.com>
2021-03-06 18:28:41 +0200

    delete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
 import './app.css';
-// import Game from './Game.js';
 import Settings from './Settings.js'
 
 function App() {
   return (
     <div className='App'>
         <h1>Match game</h1>
-      {/*<Game/>*/}
         <Settings/>
     </div>
   );
--- src/Game.js
+++ src/Game.js
@@ -45,33 +45,29 @@ export default class Game extends Component {
                     botCount: +this.state.botCount + i,
                 });
             }
-
         }, 200)
-
-
     };
 
-    componentDidUpdate =  () => {
-        setTimeout(() => {
-        if (this.state.matches === 0) {
-            if (this.state.playerCount % 2 === 0) {
-                alert('Вы победили');
-                this.setState({
-                    matches: +this.props.data.countOfMatches,
-                    playerCount: 0,
-                    botCount: 0,
-                    gameEnd: true
-                });
-            } else if (this.state.botCount % 2 === 0) {
-                alert('Вы проиграли');
-                this.setState({
-                    matches: +this.props.data.countOfMatches,
-                    playerCount: 0,
-                    botCount: 0,
-                    gameEnd: true
-                });
+    componentDidUpdate = () => {
+            if (this.state.matches === 0) {
+                if (this.state.playerCount % 2 === 0) {
+                    alert('Вы победили');
+                    this.setState({
+                        matches: +this.props.data.countOfMatches,
+                        playerCount: 0,
+                        botCount: 0,
+                        gameEnd: true
+                    });
+                } else if (this.state.botCount % 2 === 0) {
+                    alert('Вы проиграли');
+                    this.setState({
+                        matches: +this.props.data.countOfMatches,
+                        playerCount: 0,
+                        botCount: 0,
+                        gameEnd: true
+                    });
+                }
             }
-        }}, 200)
     };
 
 
@@ -87,7 +83,8 @@ export default class Game extends Component {
 
         const buttonList = this.state.matchesToTake.map((item, index) => {
             let i = index + 1;
-            return <button onClick={this.handleClick} value={i} className='buttons' disabled={this.state.matches < i || this.state.botTurn}>{i}</button>
+            return <button onClick={this.handleClick} value={i} className='buttons'
+                           disabled={this.state.matches < i || this.state.botTurn}>{i}</button>
         });
 
         return (
@@ -97,22 +94,22 @@ export default class Game extends Component {
                     <h3>{matchList}</h3>
                     <p>Сколько спичек вы хотите взять?</p>
                     <div className='buttons-list'>
-                    {buttonList}
+                        {buttonList}
                     </div>
                     <div className='players-list'>
                         <div className='players-wrapper'>
-                    <span>Ваш счет: {this.state.playerCount}</span>
-                        <p>{playerList}</p>
+                            <span>Ваш счет: {this.state.playerCount}</span>
+                            <p>{playerList}</p>
                         </div>
                         <div className='players-wrapper'>
-                    <span>Счет противника: {this.state.botCount}</span>
-                        <p>{botList}</p>
-                    </div>
+                            <span>Счет противника: {this.state.botCount}</span>
+                            <p>{botList}</p>
+                        </div>
                     </div>
                     <button onClick={() => {
                         this.props.callback()
                     }}
-                    className='settings-submit'
+                            className='settings-submit'
                     >Сменить настройки
                     </button>
                 </div>
